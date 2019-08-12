2fef5c431da6f097f310a1053cd5087cc7a06630
Lugaga <maureezgaga@gmail.com>
2019-08-12 12:40:42 +0300

    add urls application file for routing

---
+++ neighbour/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.index, name='index'),
+    url(r'^search$', views.search, name='search'),
+    url(r'^business$', views.business, name='business'),
+    url(r'^profile/(?P<username>\w{0,50})$', views.profile, name='profile'),
+    url(r'^edit_profile/(?P<username>\w{0,50})$', views.edit_profile, name='edit_profile'),
+    url(r'^post/(\d+)$', views.post, name='post'),
+    url(r'^new_post$', views.new_post, name='new_post'),
+    url(r'^new_business$', views.new_business, name='new_business')
+]
